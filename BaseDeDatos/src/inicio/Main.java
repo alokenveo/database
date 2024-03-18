719a67e4e48900deb72efa7dadeed436716a61b2
LENOVO <LENOVO@DESKTOP-4O4HO7A>
2024-03-18 18:45:55 +0100

    hola

--- BaseDeDatos/src/inicio/Main.java
+++ BaseDeDatos/src/inicio/Main.java
@@ -8,7 +8,6 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hola");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
