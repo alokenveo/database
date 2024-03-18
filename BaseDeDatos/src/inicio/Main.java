540402cba6a6589f75072283569104a72d546f93
LENOVO <LENOVO@DESKTOP-4O4HO7A>
2024-03-18 18:34:27 +0100

    Funciona

--- BaseDeDatos/src/inicio/Main.java
+++ BaseDeDatos/src/inicio/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+            System.out.println();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
